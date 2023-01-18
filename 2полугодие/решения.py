779229493ffa4e74e28d53f0931c78e97dcac78f
Super_Evrei2005 <54744048+EzMaxGame@users.noreply.github.com>
2023-01-18 14:45:26 +0700

    Update решения.py

--- 2полугодие/решения.py
+++ 2полугодие/решения.py
@@ -15,11 +15,11 @@ for i in range(1,100):
 from turtle import *
 left(90)
 for i in range(7):
-forward(300)
-right(120)
+    forward(300)
+    right(120)
 pu()
 for x in range(1,9):
-for y in range(1,10):
-goto(x*30,y*30)
-dot(5)
+    for y in range(1,10):
+        goto(x*30,y*30)
+        dot(5)
 done()
